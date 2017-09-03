e19d2eae3f8ba42d974730503a7ddc8c34807afb
GeoMod <olearymn@gmail.com>
2017-09-03 17:46:40 -0500

    Breweries added

--- MNCraftBeerWatcher WatchKit Extension/BreweryData.swift
+++ MNCraftBeerWatcher WatchKit Extension/BreweryData.swift
@@ -108,12 +108,72 @@ let BrokenClock = BreweryData(breweryName: "Brocken Clock", location: "Minneapol
 let BurningBros = BreweryData(breweryName: "Burning Brothers", location: "St. Paul", latitude: 44.958781, longitude: -93.173987, sun: "12-6p", mon: "Closed", tue: "Closed", wed: "4p-10p", thur: "4p-10p", fri: "2p-10p", sat: "12p-10p", rating: "")
     // 1750 Thomas Ave. St. Paul, MN 55104
     // 651-444-8882
+let CanalPark = BreweryData(breweryName: "Canal Park Brewing", location: "Duluth", latitude: 46.784771, longitude: -92.093932, sun: "11a-11p", mon: "11a-11p", tue: "11a-11p", wed: "11a-11p", thur: "11a-11p", fri: "11a-12a", sat: "11a-12a", rating: "")
+    // 300 Canal Park Drive Duluth, MN 55802
+    //  218-464-4790
+let CarmodyIrish = BreweryData(breweryName: "Carmody Irish", location: "Duluth", latitude: 46.789897, longitude: -92.093580, sun: "11a-2a", mon: "3p-2a", tue: "3p-2a", wed: "3p-2a", thur: "3p-2a", fri: "3p-2a", sat: "11a-2a", rating: "")
+    // 308 E Superior St Duluth, MN 55802
+    // 218-740-4747
+let CastleDanger = BreweryData(breweryName: "Castle Danger", location: "Two Harbors", latitude: 47.019550, longitude: -91.673055, sun: "12p-9p", mon: "12p-9p", tue: "12p-9p", wed: "12p-9p", thur: "12p-9p", fri: "12p-10p", sat: "12p-10p", rating: "")
+    // 17 Seventh St. Two Harbors, MN 55616
+    // 218-834-5800
+let Clockwerks = BreweryData(breweryName: "Clockwerks Brewing", location: "Minneapolis", latitude: 44.980895, longitude: -93.271640, sun: "12p-8p", mon: "3p-10p", tue: "3p-10p", wed: "3p-10p", thur: "3p-10p", fri: "3p-12a", sat: "12p-12a", rating: "")
+    // 25 NORTH 4TH STREET, MINNEAPOLIS, MN, 55401
+    // 612-339-9375
+let Cosmos = BreweryData(breweryName: "Cosmos Brewing", location: "Hugo", latitude: 45.152687, longitude: -92.912253, sun: "Closed", mon: "Closed", tue: "Closed", wed: "Closed", thur: "Closed", fri: "Closed", sat: "Closed", rating: "")
+let DangerousMan = BreweryData(breweryName: "Dangerous Man", location: "Minneapolis", latitude: 45.001056, longitude: -93.266555, sun: "12p-8p", mon: "Closed", tue: "4p-10p", wed: "4p-10p", thur: "4p-10p", fri: "3p-12a", sat: "12p-12a", rating: "")
+    // 1300 Second St. NE Minneapolis, MN 55413
+let DayBlock = BreweryData(breweryName: "Day Block Brewing", location: "Minneapolis", latitude: 44.975178, longitude: -93.252732, sun: "10a-11p", mon: "11a-11p", tue: "11a-11p", wed: "11a-11p", thur: "11a-1a", fri: "11a-1a", sat: "11a-1a", rating: "")
+    // 1105 Washington Ave. S. Minneapolis, MN 55415
+    // 612-617-7793
+let Disgruntled = BreweryData(breweryName: "Disgruntled Brewing", location: "Perham", latitude: 46.590527, longitude: -95.558947, sun: "12p-6p", mon: "Closed", tue: "3p-10p", wed: "3p-10p", thur: "3p-10p", fri: "11a-11p", sat: "11a-11p", rating: "")
+    // 735 2nd St NE Perham, MN
+    // 218-346-4677
+let EastLake = BreweryData(breweryName: "Eastlake Craft", location: "Minneapolis", latitude: 44.948571, longitude: -93.260151, sun: "12p-11p", mon: "12p-11p", tue: "12p-11p", wed: "12p-11p", thur: "12p-11p", fri: "12p-11p", sat: "12p-11p", rating: "")
+    // 920 E. Lake St. #123 Minneapolis, MN 55407
+    // 612-224-9713
+let Enki = BreweryData(breweryName: "Enki Brewing", location: "Victoria", latitude: 44.861266, longitude: 44.861266, sun: "12p-7p", mon: "Closed", tue: "4p-10p", wed: "4p-10p", thur: "4p-10p", fri: "2p-10p", sat: "12p-10p", rating: "")
+    // 1495 Stieger Lake Lane Victoria, MN 55386
+    // 952.300.8408
+let Excelsior = BreweryData(breweryName: "Excelsior Brewing", location: "Excelsior", latitude: 44.902333, longitude: -93.566429, sun: "12p-6p", mon: "Closed", tue: "4p-9p", wed: "4p-10p", thur: "4p-10p", fri: "12p-11p", sat: "12-11p", rating: "")
+    // 421 Third St. Excelsior, MN 55331
+    // 952-474-7837
+let FTown = BreweryData(breweryName: "F-Town Brewing", location: "Faribault", latitude: 44.294994, longitude: -93.266781, sun: "12p-3p", mon: "Closed", tue: "Closed", wed: "3p-9p", thur: "3p-9p", fri: "3p-10p", sat: "12p-10p", rating: "")
+let FairState = BreweryData(breweryName: "Fair State", location: "Minneapolis", latitude: 45.013618, longitude: -93.247448, sun: "12p-9p", mon: "4p-11p", tue: "4p-11p", wed: "4p-11p", thur: "4p-11p", fri: "2p-12a", sat: "12p-12a", rating: "")
+    // 2506A Central Ave. NE Minneapolis, MN 55418
+    // 612.444.3209
+let Finnegans = BreweryData(breweryName: "Finnegans Brew", location: "Minneapolis", latitude: 44.970085, longitude: -93.265828, sun: "Closed", mon: "Closed", tue: "Closed", wed: "Closed", thur: "Closed", fri: "Closed", sat: "Closed", rating: "")
+    // 609 South 10th Street, Suite 102 Minneapolis, MN 55404
+let Fitgers = BreweryData(breweryName: "Fitger's Brewhouse", location: "Duluth", latitude: 46.792418, longitude: -92.090448, sun: "11a-Close", mon: "11a-Close", tue: "11a-Close", wed: "11a-Close", thur: "11a-Close", fri: "11a-Close", sat: "11a-Close", rating: "")
+    // 600 E. Superior St. Duluth, MN 55802
+    // (218) 279-2739
+let FlatEarth = BreweryData(breweryName: "Flat Earth", location: "St. Paul", latitude: 44.962767, longitude: -93.071162, sun: "12p-6:30p", mon: "Closed", tue: "3:30p-9:30p", wed: "3:30p-8p", thur: "3:30p-10p", fri: "3:30p-11p", sat: "12p-11p", rating: "")
+    // 688 Minnehaha Ave. E. St. Paul, MN 55106
+    // 651-698-1945
+let Forager = BreweryData(breweryName: "Forager Brewery", location: "Rochester", latitude: 44.029562, longitude: -92.477656, sun: "10a-11p", mon: "11a-11p", tue: "11a-11p", wed: "11a-11p", thur: "11a-11p", fri: "11a-11p", sat: "11a-11p", rating: "")
+    // 2105 Sixth St. NW Rochester, MN 55901
+    // 507-258-7490
+let FoxHole = BreweryData(breweryName: "Foxhole Brewhouse", location: "Wilmar", latitude: 45.120706, longitude: -95.047372, sun: "1p-4p", mon: "Closed", tue: "Closed", wed: "4p-9p", thur: "4p-9p", fri: "3p-10p", sat: "2p-10p", rating: "")
+    // 313 Fourth St. SW Willmar, MN 56201
+    // 320-441-2071
+let Fulton = BreweryData(breweryName: "Fulton Beer", location: "Minneapolis", latitude: 44.984914, longitude: -93.278656, sun: "12p-6p", mon: "Closed", tue: "3p-10p", wed: "3p-10p", thur: "11a-10p", fri: "3p-11p", sat: "12p-11p", rating: "")
+    // 414 Sixth Ave. N. Minneapolis, MN 55401
+    // 612-333-3208
+let GoatRidge = BreweryData(breweryName: "Goat Ridge", location: "New London", latitude: 45.301113, longitude: -94.944516, sun: "12p-6p", mon: "Closed", tue: "Closed", wed: "3p-9p", thur: "3p-10p", fri: "12p-11p", sat: "12p-11p", rating: "")
+    // 17 Central Ave. W. New London, MN 56273
+    // 320.354.2383
+let GrandRounds = BreweryData(breweryName: "Grand Rounds", location: "Rochester", latitude: 44.020302, longitude: -92.463152, sun: "4p-8p", mon: "11p-10p", tue: "11p-10p", wed: "11p-10p", thur: "11p-12a", fri: "11p-12a", sat: "11p-12a", rating: "")
+    // 4 Third St. SW Rochester, MN 55902
+let GreatWaters = BreweryData(breweryName: "Great Waters", location: "St. Paul", latitude: 44.946740, longitude: -93.096097, sun: "11a-2a", mon: "11a-2a", tue: "11a-2a", wed: "11a-2a", thur: "11a-2a", fri: "11a-2a", sat: "11a-2a", rating: "")
+    // 426 St. Peter St. St. Paul, MN 55102
+let GullDam = BreweryData(breweryName: "Gull Dam", location: "Nisswa", latitude: 46.497667, longitude: -94.297032, sun: "12p-5p", mon: "Closed", tue: "Closed", wed: "12p-10p", thur: "12p-10p", fri: "12p-10p", sat: "12p-10p", rating: "")
+    // 23836 Smiley Road Nisswa, MN 56468
+    // 218-963-2739
 
 
-// Update from here.
 
 
-var allBreweries = [ TenKBrewing, FiveSix, SixOneTwo, Able, Alloy, Shell, BadHabit, BadWeather, BadgerHill, BaldMan, Bang, Bank, BarleyJohns, BarrelTheory, Bauhaus, BeaverIsland, Bemidji, BentBrew, BentPaddle, BigAxe, BigWood, Birch, Blacklist, Boathouse, BoomIsland, Brau, BrokenClock, BurningBros ]
+var allBreweries = [ TenKBrewing, FiveSix, SixOneTwo, Able, Alloy, Shell, BadHabit, BadWeather, BadgerHill, BaldMan, Bang, Bank, BarleyJohns, BarrelTheory, Bauhaus, BeaverIsland, Bemidji, BentBrew, BentPaddle, BigAxe, BigWood, Birch, Blacklist, Boathouse, BoomIsland, Brau, BrokenClock, BurningBros, CanalPark, CarmodyIrish, CastleDanger, Clockwerks, Cosmos, DangerousMan, DayBlock, Disgruntled, EastLake, Enki, Excelsior, FTown, FairState, Finnegans, Fitgers, FlatEarth, Forager, FoxHole, Fulton, GoatRidge, GrandRounds, GreatWaters, GullDam ]
 
 var breweryIdentifier = 0
 var currentBrewery = String()
