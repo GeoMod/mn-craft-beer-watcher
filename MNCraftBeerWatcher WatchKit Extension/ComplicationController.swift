b38a350ac7dca5ed71985284769afb59f3396945
GeoMod <olearymn@gmail.com>
2017-09-06 20:38:19 -0500

    Logo updated. App tested and ready for submission.

--- MNCraftBeerWatcher WatchKit Extension/ComplicationController.swift
+++ MNCraftBeerWatcher WatchKit Extension/ComplicationController.swift
@@ -30,11 +30,42 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     // MARK: - Timeline Population
-    
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
+        switch complication.family {
+        case .modularSmall:
+            let modularSmall = CLKComplicationTemplateModularSmallSimpleText()
+            modularSmall.textProvider = CLKSimpleTextProvider(text: "MN🍺")
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date.init(timeIntervalSinceNow: 0), complicationTemplate: modularSmall)
+            handler(timelineEntry)
+        case .modularLarge:
+            // This is not displaying
+            let modularLarge = CLKComplicationTemplateModularLargeStandardBody()
+            modularLarge.headerTextProvider = CLKSimpleTextProvider(text: "MN Craft Breweries")
+            modularLarge.body1TextProvider = CLKSimpleTextProvider(text: "Enjoy Minnesota")
+            modularLarge.body2TextProvider = CLKSimpleTextProvider(text: "Craft Beer")
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date.init(timeIntervalSinceNow: 0), complicationTemplate: modularLarge)
+            handler(timelineEntry)
+        case .circularSmall:
+            let circularSmall = CLKComplicationTemplateCircularSmallSimpleText()
+            circularSmall.textProvider = CLKSimpleTextProvider(text: "MN🍺")
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date.init(timeIntervalSinceNow: 0), complicationTemplate: circularSmall)
+            handler(timelineEntry)
+        case .utilitarianSmall:
+            let utilitarianSmall = CLKComplicationTemplateUtilitarianSmallFlat()
+            utilitarianSmall.textProvider = CLKSimpleTextProvider(text: "MN🍺")
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date.init(timeIntervalSinceNow: 0), complicationTemplate: utilitarianSmall)
+            handler(timelineEntry)
+        case .utilitarianLarge:
+            let utilitarianLarge = CLKComplicationTemplateUtilitarianLargeFlat()
+            utilitarianLarge.textProvider = CLKSimpleTextProvider(text: "MN Breweries", shortText: "Breweries")
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date.init(timeIntervalSinceNow: 0), complicationTemplate: utilitarianLarge)
+            handler(timelineEntry)
+        default:
+            print("Default")
+        }
         handler(nil)
     }
+    
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries prior to the given date
@@ -47,9 +78,34 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     // MARK: - Placeholder Templates
-    
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
+        
+        switch complication.family {
+        case .modularSmall:
+            let modularSmall = CLKComplicationTemplateModularSmallSimpleText()
+            modularSmall.textProvider = CLKSimpleTextProvider(text: "MN🍺")
+            handler(modularSmall)
+        case .modularLarge:
+            let modularLarge = CLKComplicationTemplateModularLargeStandardBody()
+            modularLarge.headerTextProvider = CLKSimpleTextProvider(text: "MN Craft Breweries")
+            modularLarge.body1TextProvider = CLKSimpleTextProvider(text: "Enjoy Minnesota")
+            modularLarge.body2TextProvider = CLKSimpleTextProvider(text: "Craft Beer")
+            handler(modularLarge)
+        case .utilitarianSmall:
+            let utilitarianSmall = CLKComplicationTemplateUtilitarianSmallFlat()
+            utilitarianSmall.textProvider = CLKSimpleTextProvider(text: "MN🍺")
+            handler(utilitarianSmall)
+        case .utilitarianLarge:
+            let utilitarianLarge = CLKComplicationTemplateUtilitarianLargeFlat()
+            utilitarianLarge.textProvider = CLKSimpleTextProvider(text: "MN Breweries", shortText: "Breweries")
+            handler(utilitarianLarge)
+        case .circularSmall:
+            let circularSmall = CLKComplicationTemplateCircularSmallSimpleText()
+            circularSmall.textProvider = CLKSimpleTextProvider(text: "MN🍺")
+            handler(circularSmall)
+        default:
+            break
+        }
         handler(nil)
     }
     
